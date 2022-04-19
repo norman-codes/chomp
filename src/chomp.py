b8d3081007adb9035ac1eb8655610d5e6c2f1c79
norman-codes <nbukingolts@ufl.edu>
2022-04-19 11:53:39 -0400

    Introduced "Chompability" (location score to be sorted).
    Custom rating metric for our project introduced - "Chompability" - based on the number of reviews a Restaurant has, the number of stars it has, and its distance to UF. Also implemented function to recalculate it based on user's comfort with larger distance.

--- src/chomp.py
+++ src/chomp.py
@@ -9,6 +9,21 @@ import json
 # distance between two latitude/longitude pairs.
 import math
 
+# DISTANCE CALCULATION:
+# The latitude and longitude coordinate pair of the University of Florida, extracted from Google Maps:
+# https://www.google.com/maps/place/University+of+Florida/@29.6436325,-82.3636849,15z/data=!4m8!1m2!3m1!2sUniversity+of+Florida!3m4!1s0x88e8a30cfbe49275:0x206fe0de143d9886!8m2!3d29.6436325!4d-82.3549302
+UF_latitude = 29.6436325
+UF_longitude = -82.3636849
+# The Haversine formula (https://en.wikipedia.org/wiki/Haversine_formula)
+# is used to calculate the distance between two coordinates in "WGS-84" format.
+# https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+
+def distance(latitude_1, longitude_1, latitude_2, longitude_2):
+    p = math.pi / 180 # PI / 180 is to convert degrees to radians.
+    a = 0.5 - (math.cos((latitude_2 - latitude_1) * p) / 2) + (math.cos(latitude_1 * p)) * (math.cos(latitude_2 * p)) * ((1 - math.cos((longitude_2 - longitude_1) * p)) / 2)
+    return math.ceil(12742 * math.asin(math.sqrt(a))) # Returns distance between two coordinate pairs in KILOMETERS, rounded up to nearest integer (ceiling). (note that 12742 = diameter of the Earth in km.)
+# usage: var = distance(obj.latitude, obj.longitude, UF_latitude, UF_longitude)
+
 # CLASS: Location
 # This class ontains relevant information for each location within the Yelp dataset, namely:
     # business ID (string),
@@ -37,14 +52,12 @@ class Location:
 
 
     # "Chompability" - the custom rating of a location based on:
-        # distance from UF to the location
-            # calculated from longitude/latitude pairs
-        # (numReviews / stars)
-    chompability = 0
+    chompability = 0.0
 
     # "Constructor"
     def __init__(self, _ID, _name, _address, _city, _state, _latitude, _longitude, _stars, _numReviews, _categories):
         # Setting the object's values to the parsed .json's values.
+        
         self.ID = _ID
         self.name = _name
         self.address = _address
@@ -57,21 +70,12 @@ class Location:
         self.categories = _categories
         
         # Calculating "Chompability":
+        # chompability = (X / distance) + (numReviews / stars)
+        # where X is a *USER-INPUTTED* "closeness factor" (default is 1);
+        # if it is increased, then the user is okay with traveling a larger distance
+        # and as a result, the chompability of restaurants that are further away from UF will increase.
 
-# DISTANCE CALCULATION:
-# The latitude and longitude coordinate pair of the University of Florida, extracted from Google Maps:
-# https://www.google.com/maps/place/University+of+Florida/@29.6436325,-82.3636849,15z/data=!4m8!1m2!3m1!2sUniversity+of+Florida!3m4!1s0x88e8a30cfbe49275:0x206fe0de143d9886!8m2!3d29.6436325!4d-82.3549302
-UF_latitude = 29.6436325
-UF_longitude = -82.3636849
-# The Haversine formula (https://en.wikipedia.org/wiki/Haversine_formula)
-# is used to calculate the distance between two coordinates in "WGS-84" format.
-# https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
-
-def distance(latitude_1, longitude_1, latitude_2, longitude_2):
-    p = math.pi / 180 # PI / 180 is to convert degrees to radians.
-    a = 0.5 - (math.cos((latitude_2 - latitude_1) * p) / 2) + (math.cos(latitude_1 * p)) * (math.cos(latitude_2 * p)) * ((1 - math.cos((longitude_2 - longitude_1) * p)) / 2)
-    return math.ceil(12742 * math.asin(math.sqrt(a))) # Returns distance between two coordinate pairs in KILOMETERS, rounded up to nearest integer (ceiling). (note that 12742 = diameter of the Earth in km.)
-# usage: var = distance(obj.latitude, obj.longitude, UF_latitude, UF_longitude)
+        self.chompability = (1 / distance(_latitude, _longitude, UF_latitude, UF_longitude)) + (_numReviews / _stars)
 
 # READING THE .json FILE
 # (and creating Location objects with it, to be pushed into an array.)
