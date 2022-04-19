12fefa1c6b6aae2458116b5028a70e5afe6c3eb8
blakeloiacono <blakeloiacono@ufl.edu>
2022-04-19 14:23:17 -0400

    Quick sort
    Implemented Quick sort function

--- src/chomp.py
+++ src/chomp.py
@@ -106,7 +106,56 @@ for encoding_type in types_of_encoding:
 # QUICKSORT
 # Reference: https://www.geeksforgeeks.org/python-program-for-quicksort/
 
-#commit test
+# This function takes last element as pivot, places
+# the pivot element at its correct position in sorted
+# array, and places all smaller (smaller than pivot)
+# to left of pivot and all greater elements to right
+# of pivot
+
+def partition(location_arr, low, high):
+	i = (low-1)		 # index of smaller element
+	pivot = location_arr[high]	 # pivot
+
+	for j in range(low, high):
+
+		# If current element is smaller than or
+		# equal to pivot
+		if location_arr[j] <= pivot:
+
+			# increment index of smaller element
+			i = i+1
+			location_arr[i], location_arr[j] = location_arr[j], location_arr[i]
+
+	location_arr[i+1], location_arr[high] = location_arr[high], location_arr[i+1]
+	return (i+1)
+
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low --> Starting index,
+# high --> Ending index
+
+# Function to do Quick sort
+
+
+def quickSort(location_arr, low, high):
+	if len(location_arr) == 1:
+		return location_arr
+	if low < high:
+
+		# pi is partitioning index, arr[p] is now
+		# at right place
+		pi = partition(location_arr, low, high)
+
+		# Separately sort elements before
+		# partition and after partition
+		quickSort(location_arr, low, pi-1)
+		quickSort(location_arr, pi+1, high)
+
+
+# Driver code to test above
+location_arr = [10, 7, 8, 9, 1, 5]
+n = len(location_arr)
+quickSort(location_arr, 0, n-1)
 
 # HEAPIFY
 # Reference: https://www.geeksforgeeks.org/python-program-for-heap-sort/
